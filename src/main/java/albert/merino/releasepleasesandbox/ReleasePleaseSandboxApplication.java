2060832581f659318f778015aacd41e9e2af987f
Albert Merino <albert.merino@ibm.com>
2026-03-30 15:57:08 +0200

    chore: Initial commit

---
+++ src/main/java/albert/merino/releasepleasesandbox/ReleasePleaseSandboxApplication.java
@@ -0,0 +1,13 @@
+package albert.merino.releasepleasesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasePleaseSandboxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReleasePleaseSandboxApplication.class, args);
+  }
+
+}
---
+++ src/test/java/albert/merino/releasepleasesandbox/ReleasePleaseSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package albert.merino.releasepleasesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleasePleaseSandboxApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

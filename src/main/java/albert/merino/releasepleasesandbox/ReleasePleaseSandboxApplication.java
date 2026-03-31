7f97da1ad0290e77cb8f40d5b5a729865b510e4e
Albert Merino <albert.merino@ibm.com>
2026-03-31 13:30:52 +0200

    fix: add debug output to test method

--- src/main/java/albert/merino/releasepleasesandbox/ReleasePleaseSandboxApplication.java
+++ src/main/java/albert/merino/releasepleasesandbox/ReleasePleaseSandboxApplication.java
@@ -14,6 +14,7 @@ public class ReleasePleaseSandboxApplication {
     int a = 1;
     int b = 2;
     int c = a + b; // Fixed: now using the calculated sum
+    System.out.println("Result: " + c); // Fixed: added output for debugging
   }
 
 }
